8dac4f1178791967ad8a098fa9a813e71ee11b0d
Davide Filosa <davideclaypool@gmail.com!>
2023-10-14 13:08:37 +0200

    fixed bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,11 +24,12 @@ export default function RootLayout({
   return (
     <>
       <Script
+        id="my-script"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy="lazyOnload">
+      <Script id="my-script2" strategy="lazyOnload">
         {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
