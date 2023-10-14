b640f4402ee7de46cdb1b433b70a741c299f5c75
Davide Filosa <davideclaypool@gmail.com!>
2023-10-14 16:44:31 +0200

    fixed bug swcMinify

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: false,
+
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
