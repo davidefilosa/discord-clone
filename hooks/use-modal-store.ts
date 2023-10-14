61085d5b0ed3d8ce9712d85be02bd1744de6f202
Davide Filosa <davideclaypool@gmail.com!>
2023-10-14 09:53:31 +0200

    fixed bug

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { Server } from "@prisma/client";
+import { Channel, ChannelType, Server } from "@prisma/client";
 
 export type ModalType =
   | "createServer"
@@ -16,6 +16,10 @@ export type ModalType =
 
 interface ModalData {
   server?: Server;
+  channel?: Channel;
+  channelType?: ChannelType;
+  apiUrl?: string;
+  query?: Record<string, any>;
 }
 
 interface ModalStore {
