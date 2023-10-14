91b5d2eb956f4b81e5376816a1f83eb5aa47c379
Davide Filosa <davideclaypool@gmail.com!>
2023-10-14 10:02:27 +0200

    fixed bug

--- components/modals/delete-channel-modal.tsx
+++ components/modals/delete-channel-modal.tsx
@@ -32,7 +32,7 @@ const DeleteChannelModal = () => {
     try {
       setIsLoading(true);
       const url = qs.stringifyUrl({
-        url: `/api/channels/${channel.id}`,
+        url: `/api/channels/${channel?.id}`,
         query: {
           serverId: params?.serverId,
         },
