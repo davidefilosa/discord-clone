c2794fe5901db8bcc7fc2319a119970cdc6ae232
Davide Filosa <davideclaypool@gmail.com!>
2023-10-14 15:42:34 +0200

    fixed bug with swc

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: false,
+
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
       bufferutil: "commonjs bufferutil",
-      swcMinify: false,
     });
 
     return config;
   },
+
   images: {
     remotePatterns: [
       {
